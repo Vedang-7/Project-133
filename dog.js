04a1baa9c4f383d90b7c8111edf73616910e2ae5
Vedang-7 <86293705+Vedang-7@users.noreply.github.com>
2022-06-12 17:47:04 +0530

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,27 @@
+status="";  
+function preload(){
+    img=loadImage("Dog&cat.jpg");
+}
+function setup() {
+    canvas = createCanvas(540, 320);
+    canvas.center();
+    object_detector= ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+    object_detector.detect(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+function back(){
+    window.location="index.html"
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function tp1(){
+    window.location="dog.html"  
+}
+function tp2(){
+    window.location="apple.html"  
+}
+function tp3(){
+    window.location="television.html"  
+}
+function tp4(){
+    window.location="wardrobe.html"  
+}
+function tp5(){
+    window.location="computer.html"  
+}
